aed000c0fcd7d60d89597a501e39edf0d0a7afdb
Michael07220823 <michael31703@gmail.com>
2020-11-16 10:38:35 +0800

    Add example code.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+# readme.md = github readme.md, 這裡可接受markdown寫法
+# 如果沒有的話，需要自己打出介紹此專案的檔案，再讓程式知道
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hello_test01", # 
+    version="0.0.1",
+    author="overcomer",
+    author_email="michael31703@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Michael07220823/first_pypi_project_example.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
